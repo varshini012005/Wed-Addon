954ef19897d8268068107c15126b801c865ee3fd
varshini012005 <2326ka50@kgcas.com>
2025-06-12 12:49:39 +0530

    Create Execute.java

---
+++ day6/Execute.java
@@ -0,0 +1,13 @@
+class Main extends Thread{
+    public void run(){
+        for(int i=1;i<=10;i++){
+        System.out.println("Hai Thread" +" "+i);
+        }
+    }
+}
+class Execute{
+    public static void main(String[] args){
+        Main m=new Main();
+        m.start();
+    }
+}
