25db95d9c6aa0e86030d8d48c0e277b04491a65e
varshini012005 <2326ka50@kgcas.com>
2025-06-19 14:44:37 +0530

    Create task.java

---
+++ dAY7/task.java
@@ -0,0 +1,58 @@
+  mysql> create table Course(
+    -> Coursefee int
+    -> );
+mysql> insert into course values(10000);
+mysql> insert into course values(20000);
+mysql> insert into course values(30000);
+mysql> insert into course values(40000);
+mysql> insert into course values(50000);
+mysql> select*
+    -> from course;
++-----------+
+| Coursefee |
++-----------+
+|     10000 |
+|     20000 |
+|     30000 |
+|     40000 |
+|     50000 |
++-----------+
+
+
+
+TASK 3
+mysql> create table Course(
+    -> Coursefee int
+    -> );
+
+mysql> insert into course values(10000);
+mysql> insert into course values(20000);
+mysql> insert into course values(30000);
+mysql> insert into course values(40000);
+mysql> insert into course values(50000);
+mysql> select*
+    -> from course;
++-----------+
+| Coursefee |
++-----------+
+|     10000 |
+|     20000 |
+|     30000 |
+|     40000 |
+|     50000 |
++-----------+
+   
+mysql> update course
+    -> set coursefee=60000
+    -> where coursefee=50000;
+mysql> select*from course
+    -> ;
++-----------+
+| Coursefee |
++-----------+
+|     10000 |
+|     20000 |
+|     30000 |
+|     40000 |
+|     60000 |
++-----------+
